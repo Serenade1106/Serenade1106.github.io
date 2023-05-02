001c3f9a03c3d8832a129cd70a7f2c23c61439f2
Serenade1106 <serenazhengsirong@outlook.com>
2023-05-02 19:26:34 -0400

    data

---
+++ serena_cis23/project/nycdata/inner/collection.js
@@ -0,0 +1,33 @@
+// Function to render your items
+function renderItems(collection) {
+	// The `ul` where the items will be inserted
+	const collectionList = document.getElementById('collection')
+
+	// Loop through each item in the collection array
+	collection.forEach(function(item) {
+
+		// This can get annoying, so we can use “template literals” instead
+		const itemDetails =
+			`
+
+			<img src="${item.complaint_type}" />
+
+			`
+			collectionList.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
+
+	})
+}
+
+
+
+// Fetch gets your JSON file.
+fetch('collection.json')
+	.then(function(response) {
+		return response.json();	
+	})
+	.then(function(collection){
+		// And passes the data to the function, above!
+		renderItems(collection) // In reverse order
+	})
+
+	
